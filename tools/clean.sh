60323b5ddcf02d8b33f9a9579fc5f2769c176803
Bonnee <bonora.matteo@gmail.com>
2020-11-24 18:13:27 +0100

    Update clean script

--- tools/clean.sh
+++ tools/clean.sh
@@ -5,4 +5,5 @@ while read file
   if [ $(git log --follow --format=%at -- $file | tail -1) -lt $(date -dyesterday +%s) ]; then
     git rm $file
   fi
+
 done <<< $(git ls-tree -r --name-only HEAD imgs)
